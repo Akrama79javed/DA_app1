08c8c216ddf0f259277bb5a44558dd12f47e4a1d
akrama_javed <akramajaved79@gmail.com>
2023-07-08 11:12:25 +0530

    first commit

---
+++ pages/about.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+#create a little for your app
+st.title('house price Data Analysis')
+st.subheader('about the app')
+
+st.markdown("""
+this app performance simple data anlysis on the ""house price"" dataset.
+***python libraries:***
+    - pandas
+    - streamlit
+    = numpy
+    - matplotly
+    - seaborn           
+            """)
